53a23bbf0c115f906168ed6911eb3155f3dcf2ee
Kiran Kumar Reddy <rkirankumar761@gmail.com>
2024-02-06 22:51:39 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react'
+import Alert from './components/Alert';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  const changeMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = '#043a66  ';
+      showAlert("Dark Mode is Enabled", "success");
+      document.title = "TextUtils - DarkMode";
+    }
+    else {
+      setMode("light");
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode is Enabled", "success");
+      document.title = "TextUtils - LightMode";
+
+    }
+  }
+  return (
+    <>
+      
+      <Alert alert={alert} />
+      <div className="container">
+        <BrowserRouter>
+        <Navbar title="Kiran Kumar Reddy" mode={mode} changeMode={changeMode}></Navbar>
+          <Routes>
+            <Route index element={<TextForm showAlert={showAlert} heading="Enter the text" mode={mode} />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+    const [text, setText] = useState("Enter Text here")
+
+    const handleUpClick = () => {
+        // console.log("Button was clicked");
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to Upper Case","success")
+    }
+
+    const handleLoClick = () => {
+        // console.log("Button was clicked");
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleOnChange = (event) => {
+        // console.log("On Change");
+        setText(event.target.value);
+
+    }
+    const handleClear = () => {
+        // console.log("Button was clicked");
+        let newText = "";
+        setText(newText);
+    }
+
+    const wordCount =(text)=>{
+        let r=/\s+\S+/;
+        let numOfWords=text.split(r);
+        return numOfWords.length;
+    }
+
+    return (
+        <>
+            <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
+                <h2>{props.heading}</h2>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} style={{backgroundColor:props.mode==='dark'?'grey':'white' , color:props.mode==='dark'?'white':'black'}} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
+                <button className="btn btn-primary mx-2" onClick={handleClear}>Clear Text</button>
+            </div>
+            <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
+                <h2>Your Text Summary</h2>
+                <p>There are {text===""? 0 : wordCount(text)} words and {text.length} characters.</p>
+                <p>You ll need around {0.08 * text.split(" ").length} minutes to read.</p>
+                <h2>
+                    Preview
+                </h2>
+                <p>{text}</p>
+            </div>
+        </>
+    )
+}
