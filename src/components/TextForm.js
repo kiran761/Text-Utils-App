80a2fc9af3306003c373a6dea0766fcecae8b535
Kiran Kumar Reddy <rkirankumar761@gmail.com>
2024-02-07 12:14:35 +0530

    Changed Buttons

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -40,9 +40,10 @@ export default function TextForm(props) {
                 <div className="mb-3">
                     <textarea className="form-control" value={text} style={{backgroundColor:props.mode==='dark'?'grey':'white' , color:props.mode==='dark'?'white':'black'}} onChange={handleOnChange} id="myBox" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
-                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
-                <button className="btn btn-primary mx-2" onClick={handleClear}>Clear Text</button>
+                <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-2 my-2" onClick={handleClear}>Clear Text</button>
+                <button className="btn btn-primary mx-2 my-2" onClick={handleLoClick}>Convert to LowerCase</button>
+                
             </div>
             <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
                 <h2>Your Text Summary</h2>
